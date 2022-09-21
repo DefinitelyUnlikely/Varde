d4e120c0688adf786b1ba1fc363f1eeb0657db2f
Martin Johansson <martin@kaarjohansson.se>
2022-09-21 17:41:44 +0200

    added png of nested dict idea

--- varde.py
+++ varde.py
@@ -1,5 +1,15 @@
 # What do we want to achieve? First, start by creating a function that reads one file and adds it into the dictionary. We'll 
 # work on fixing the rest later. Maybe first of all, we want to actually just figure out how the dictionary should look. 
+
+# So, the current concept I have in mind is quite straigtforward to explain, but perhaps harder to implement. 
+# We simply work through each record of the table. We first read the region. We create a top level dictionary for the region
+# if one doesn't already exist. We then move forward and add the store for the record. We add that store as a dictionary as well
+# within the region dictionary. The store dictionary will then hold a dictionary as well, containing the value and sales keys. Which we increment with 
+# the value of the last column for the record and sales which we increment by 1each time the store is seen as a record.
+# We might also want to consider adding the value and sales to the region as a whole. Or create a function that 
+# calculates this for a given region if we want it. 
+
+
 from openpyxl import load_workbook
 from collections import defaultdict
 
