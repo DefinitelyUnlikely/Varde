29d1197e31147c98e75123242693f448d40e1c5f
Martin Johansson <martin@kaarjohansson.se>
2022-09-21 17:14:32 +0200

    Inital Testing

---
+++ varde.py
@@ -0,0 +1,27 @@
+# What do we want to achieve? First, start by creating a function that reads one file and adds it into the dictionary. We'll 
+# work on fixing the rest later. Maybe first of all, we want to actually just figure out how the dictionary should look. 
+from openpyxl import load_workbook
+from collections import defaultdict
+
+def main():
+    pass
+
+    def read_file():
+        workbook = load_workbook("C:\Code\Projects\Varde\9Sep.xlsx")
+        worksheet = workbook[workbook.sheetnames[1]]
+        region_column = worksheet["D"]
+        chain_column = worksheet["E"]
+        store_column = worksheet["F"]
+        value_column = worksheet["G"]
+        for i in zip(region_column[1:], chain_column[1:], store_column[1:], value_column[1:]):
+            for j in i:
+                print(str(j.value).encode('utf-8'))
+
+    value_dict = defaultdict(dict)
+    
+    read_file()
+
+
+
+if __name__ == '__main__':
+    main()
