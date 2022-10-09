10c6acbbabcb007a7277244ddc4d7fc651c3abd5
Martin Johansson <martin@kaarjohansson.se>
2022-10-09 14:18:26 +0200

    just trying out the csv

--- vardeCSV.py
+++ vardeCSV.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+
+csv_file = pd.read_csv("Projects\Varde\Analysis - topup.csv", converters={'MSISDN': str})   # Convert to keep leading zero. Might not be neccesery. 
+
+for index, row in csv_file.iterrows():
+    print(row["MSISDN"], row["Amount paid"])
