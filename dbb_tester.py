8ec6f3057b903167c6283e2110c9fe7fa25cbe61
Martin Johansson <Martin@kaarjohansson.se>
2022-10-16 18:34:50 +0200

    inner join statement for easier datafetching

--- dbb_tester.py
+++ dbb_tester.py
@@ -9,4 +9,12 @@ r'DBQ=C:\Code\Projects\master_database.accdb;'
 cursor = conn.cursor()
 
 
-ladd = cursor.execute(f'SELECT * FROM Laddningsdata WHERE "Topup date" between #10/3/22# and #10/3/22#;')
+cursor.execute('SELECT * FROM Laddningsdata INNER JOIN Storecheck ON Laddningsdata.MSISDN=Storecheck.Number WHERE "Topup date" between #10/3/22# and #10/3/22#;')
+
+for i in cursor.fetchall():
+    print(i.MSISDN, i.Activated, i.Store)
+
+# ladd = cursor.execute(f'SELECT * FROM Laddningsdata WHERE "Topup date" between #10/3/22# and #10/3/22#;')
+
+# stores = cursor.execute(f"SELECT * FROM Storecheck WHERE Number IN ({', '.join(str(i.MSISDN) for i in ladd.fetchall())});")   
+
