43772b571accd1d06583bf638e103506dd7dd740
Martin Johansson <Martin@kaarjohansson.se>
2023-02-04 16:30:17 +0100

    making some makeshift structure for exporting and breaking down functions

--- accdb_analyzer_class.py
+++ accdb_analyzer_class.py
@@ -6,10 +6,27 @@ from tkinter import filedialog
 from tkinter import ttk
 from tkcalendar import Calendar
 from collections import defaultdict, Counter
+import pandas as pd
 
 
 
 class DatabaseAnalyzer():
+    
+    
+    def __init__(self):
+        
+    
+        self.empty_cards = {'TA81218 - Telenor Prepaid TripleSIM 0kr', 'TA81258 - Telenor Prepaid TripleSIM 0kr (till 25-pack)'}
+        self.preloaded_cards = {
+            'TA81228 - Telenor Prepaid TripleSIM Fast 1 m�nad Mini',
+            'TA81259 - Telenor MBB 100 GB 1 �r',
+            'TA81220 - Telenor Prepaid TripleSIM Fast 1 m�nad',
+            'TA81235 - Telenor Prepaid MBB 10Gb',
+            'TA81230 - Telenor Prepaid TripleSIM Halv�r',
+            'TA81247 - Prepaid Startpaket HELLO',
+                    }
+        self.volvo_cards = {'TA81199 - Telenor MBB Volvo 5GB', }
+    
     
     def connect_db(self):
         file_path = filedialog.askopenfilename()
@@ -42,17 +59,6 @@ class DatabaseAnalyzer():
 
     def calculate_option(self):
         
-        empty_cards = {'TA81218 - Telenor Prepaid TripleSIM 0kr', 'TA81258 - Telenor Prepaid TripleSIM 0kr (till 25-pack)'}
-        preloaded_cards = {
-            'TA81228 - Telenor Prepaid TripleSIM Fast 1 m�nad Mini',
-            'TA81259 - Telenor MBB 100 GB 1 �r',
-            'TA81220 - Telenor Prepaid TripleSIM Fast 1 m�nad',
-            'TA81235 - Telenor Prepaid MBB 10Gb',
-            'TA81230 - Telenor Prepaid TripleSIM Halv�r',
-            'TA81247 - Prepaid Startpaket HELLO',
-                    }
-        volvo_cards = {'TA81199 - Telenor MBB Volvo 5GB', }
-        
         
         def update_table(self):
             """
@@ -73,13 +79,12 @@ class DatabaseAnalyzer():
                     if i[0].isdigit():
                         self.cursor.execute("INSERT INTO Updated_Store (MSISDN, Region, Activated, Store) VALUES (?, ?, ?, ?);", (i[0], i[3], i[2], i[5]))
             
-            startLabel['text'] = "Klar med uppdatering av regioner"            
+            startLabel['text'] = "Klar med uppdatering av regioner"  
+            
         
-        
-        def longterm(self):
+        def create_joined_table_longterm(self):
             """
-            calculates the longterm/total value of all top ups for a given period. "longterm" is including 
-            all top ups on cards less than a year old, including cards getting their first top up.
+            Calls relevent SQL queries on the database, to join together the required data for further extraction by other functions.
             """
             
             # If we have created an updated table for stores and regions, we use this query.
@@ -99,8 +104,17 @@ class DatabaseAnalyzer():
                 'INNER JOIN SIM_kort ON Laddningsdata.MSISDN=SIM_Kort.MSISDN '
                 f'WHERE "Topup date" between #{from_cal.get_date()}# and #{to_cal.get_date()}#'
                 f'AND Activated between #{earlier_string}# and #{to_cal.get_date()}# '
-                )
+                )          
+        
+        
+        def longterm(self):
+            """
+            calculates the longterm/total value of all top ups for a given period. "longterm" is including 
+            all top ups on cards less than a year old, including cards getting their first top up.
+            """
             
+            # Start by making sure the right table is in our cursor, before iterating.
+            create_joined_table_longterm(self)
 
             self.region_map = Counter()
             self.region_preloaded_map = Counter()
@@ -111,12 +125,16 @@ class DatabaseAnalyzer():
                 paid = i.__getattribute__('Amount paid')
                 self.region_map[i.Region] += paid
                 self.store_map[i.Store] += paid
-                if i.Artikel in preloaded_cards:
+                if i.Artikel in self.preloaded_cards:
                     self.region_preloaded_map[i.Region] += paid
                     self.store_preloaded_map[i.Store] += paid
+                    
+            self.long_term_df = pd.DataFrame.from_dict(self.region_map, orient='index')
 
             doneLabel = tk.Label(text="Klar med kalkyl, programmet kan stängas")
             doneLabel.place(x=50, y=350)
+            
+            
 
  
         def first_charge(self):
@@ -128,28 +146,37 @@ class DatabaseAnalyzer():
         
         
         def gross_adds(self):
-            pass
+            """
+            Calculates the amount of added RGUs for a given period.
+            """
+             
             
-        # If the attribute exists, a csv has been imported for use. 
-        if hasattr(self, 'csv_path'):
-            update_table(self)
-        
-        # Get relevant dates
+        # Get/create relevent dates in working format.
         one_year_earlier = str(int(from_cal.get_date()[-2:]) - 1)
         earlier_string = f"{from_cal.get_date()[:-2]}{one_year_earlier}"
+            
+        # If the attribute exists, a csv has been imported for use and we create an updated table. 
+        if hasattr(self, 'csv_path'):
+            update_table(self)
+            
         
         longterm(self)
-
-        for reg in self.region_map:
-            print(reg, self.region_map[reg])
-            
-        print(f"Totalt: {sum(self.region_map[reg] for reg in self.region_map)}")
+        print(self.long_term_df)
         
-        print(f"Totalt på förladdat: {sum(self.region_preloaded_map[reg] for reg in self.region_preloaded_map)}")
         
-            
+    def export_to_excel(self):
+        # Plans for this function: 
+        # Make it export everything into one excel file, with sheets for:
+        # 1. Long term value, region wise and store wise, 2. first top up value for stores and regions, 3. gross adds for stores and regions
+        
+        # I want to use pandas and write to several sheets, so we need to create an excel writer. We also need to create Pandas dataframes with the 
+        # data I want to populate my excel file with. Let's keep the creating of the dataframes in this functino for now, but they might be 
+        # more logically placed in each calulating function.
+        self.long_term_df.to_excel('testingpandas.xlsx')
 
-
+        
+        
+# Initialize an instance of the DatabaseAnalyzer class.
 db_analyzer = DatabaseAnalyzer()
 
 root = tk.Tk()
@@ -167,23 +194,26 @@ tabControl.add(calculate, text='Kalkylator')
 tabControl.add(instructions, text='Instruktioner')
 tabControl.place(x=0, y=0, width=1000, height=500 )
 
-var = tk.IntVar()
-radio1 = tk.Radiobutton(calculate, text="Regionslista", variable=var, value=1)
-radio2 = tk.Radiobutton(calculate, text="Butikslista", variable=var, value=2)
-radio1.place(x=600, y=50)
-radio2.place(x=600, y=70)
-radioLabel = tk.Label(calculate, text="Välj typ av output").place(x=600, y=20)
+# var = tk.IntVar()
+# radio1 = tk.Radiobutton(calculate, text="Regionslista", variable=var, value=1)
+# radio2 = tk.Radiobutton(calculate, text="Butikslista", variable=var, value=2)
+# radio1.place(x=600, y=50)
+# radio2.place(x=600, y=70)
+# radioLabel = tk.Label(calculate, text="Välj typ av output").place(x=600, y=20)
 
-importButton = tk.Button(calculate, text="Välj databas", command=db_analyzer.connect_db, bg="lightblue")
+importButton = tk.Button(calculate, text="Koppla databas", command=db_analyzer.connect_db, bg="lightblue")
 importButton.place(x=20, y=420)
 importButton.configure(border=2, relief="raised")
 
 csvButton = tk.Button(calculate, text="Välj csv fil", command=db_analyzer.import_csv, bg="lightblue")
-csvButton.place(x=100, y=420)
+csvButton.place(x=120, y=420)
 csvButton.configure(border=2, relief="raised")
 
 calculateButton = tk.Button(calculate, text="Kalkylera", command=db_analyzer.calculate_option, bg="lightblue")
 calculateButton.place(x=600, y=200)
+
+exportButton = tk.Button(calculate, text="Exportera", command=db_analyzer.export_to_excel, bg="lightblue")
+exportButton.place(x=930, y=200)
 
 quitButton = tk.Button(text="Exit", command=db_analyzer.quit_program, fg="mint cream", bg="DarkOrange3")
 quitButton.place(y=450, x=960)
